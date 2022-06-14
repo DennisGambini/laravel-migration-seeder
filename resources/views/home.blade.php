e8dfe8febe86f034fbabea26dc791aa3c442bf18
DennisGambini <dennis.gambini@gmail.com>
2022-06-14 16:28:31 +0200

    layout base, home e partials header e footer

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base');
+
+@section('pageTitle', 'VacatiHome')
+
+@section('mainContent')
+
+<div>ciao</div>
+<i class="fa-solid fa-home"></i>
+
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="logo">
+        <img src="https://designshop-6aa0.kxcdn.com/photos/vacanze-in-tempo-28721_15.jpg" alt="logo vacatiHome">
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{route('home')}}">Home</a>
+            </li>
+            <li>
+                <a href="{{route('holidaysList')}}">Pacchetti Vacanza</a>
+            </li>
+        </ul>
+    </nav>
+</header>
